38295504260946b9f4fecab3e7dbfaff14368f7f
Rendong Pi <89199086+Devin-Pi@users.noreply.github.com>
2025-08-28 21:36:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ with open("README.md", "r") as f:
         sys.exit("Already updated today!")
 
 # 定义搜索关键词列表
-keywords = ["Geoscience", "Spatial", "Time", "Trajectory", "City"] # TODO 添加更多关键词
+# keywords = ["Geoscience", "Spatial", "Time", "Trajectory", "City"] # TODO 添加更多关键词
+keywords = ["audio-visual localization","multi modal", "sound source localization", "acoustic robot"]
 
 max_result = 100  # 每个关键词从arXiv API查询的最大结果数
 issues_result = 15  # 在issue中包含的最大论文数量
@@ -54,7 +55,7 @@ f_is.write("title: Latest {0} Papers - {1}\n".format(issues_result, get_daily_da
 f_is.write("labels: documentation\n")  # 设置issue标签
 f_is.write("---\n")  # 写入YAML后端分隔符
 # 添加指向GitHub仓库的链接提示
-f_is.write("**Please check the [Github](https://github.com/KMnO4-zx/DailyArXiv) page for a better reading experience and more papers.**\n\n")
+f_is.write("**Please check the [Github](https://github.com/Devin-Pi/DailyArXiv) page for a better reading experience and more papers.**\n\n")
 
 # 遍历每个关键词，获取相关论文并生成表格
 for keyword in keywords:
